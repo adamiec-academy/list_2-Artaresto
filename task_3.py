ebadbcc5db24739d216de23821e38d377a6536d8
Artaresto <89027893+Artaresto@users.noreply.github.com>
2022-10-27 17:47:53 +0200

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,6 +1,20 @@
 def is_prime(n):
-    return
-
-
+    if n > 1:
+        for i in range(2, int(n/2)+1):
+            if (n % i) == 0:
+                break
+        else:
+            return n
+        n+=1 
+            
 def is_diabolic(n):
-    return
+    return "666" in str(n)
+
+def time():
+    a = 0
+    for j in range(100001):
+        if is_diabolic(j) and is_prime(j):
+            print(j)
+            a += 1
+    print (f"takich liczb jest {a}")
+print(time())
