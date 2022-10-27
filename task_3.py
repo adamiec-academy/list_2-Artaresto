95504f829e5d46b1b220744d5910d64670ed4a8b
Artaresto <89027893+Artaresto@users.noreply.github.com>
2022-10-27 18:12:03 +0200

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -2,7 +2,7 @@ def is_prime(n):
     if n > 1:
         for i in range(2, n):
             if (n % i) == 0:
-                break
+                return False
             else:
                 return True
      
