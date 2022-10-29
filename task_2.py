60d0acfe4d31976950a6af17c682089da7f9c7bb
Artaresto <89027893+Artaresto@users.noreply.github.com>
2022-10-29 15:02:07 +0200

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,1 +1,21 @@
-# Remove this comment to confirm that this task is done
+from turtle import forward, left, right, speed, penup, pendown, exitonclick 
+import random
+
+speed("slow")
+a = random.randint(1, 5)
+
+for _ in range(random.randint(5, 20)):
+    forward(20)
+    left(90)
+    forward(a)
+    left(90)
+    forward(20)
+    left(90)
+    forward(a)
+    left(90)
+    penup()
+    forward(40)
+    pendown()
+    a += random.randint(-5,  30)
+
+exitonclick()
