1cc47e484d209a13e6db63bfe3c4400bdbcd0158
Artaresto <89027893+Artaresto@users.noreply.github.com>
2022-10-30 15:42:19 +0100

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -14,7 +14,9 @@ def multiplication_table(x1, x2, y1, y2):
         print(f"{c}{b * (lenght - 1) }", end="")
 
     print(" ",end="\n")
-    print("-" * (second+(lenght -1) + x2 * (lenght - 1) - (lenght - 1)))
+    if x2*y2 > 100:
+        second -= 1
+    print("-" * ((second + 1)+ lenght + x2 * (lenght - 1) - lenght))
     
     for collumn in range(y1, y2+1):
         a = len(str(collumn))
@@ -25,4 +27,3 @@ def multiplication_table(x1, x2, y1, y2):
             free_space = lenght - a 
             outcome += " " * free_space + str(collumn*row)
         print(outcome)
-
