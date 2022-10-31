c4f1358599538c7837209c609bc76a7e24468861
Artaresto <89027893+Artaresto@users.noreply.github.com>
2022-10-31 15:50:31 +0100

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,6 +1,23 @@
 def cipher(text, shift):
+    
+    for i in range(len(text)):
+        letter = (text[i])
+        
+        if (letter.isupper()):
+            text += chr(ord(letter) + shift)
+        else:
+            text += chr(ord(letter) + shift)
+    
     return text
 
-
 def decipher(text, shift):
+
+    for i in range(len(text)):
+        letter = (text[i])
+        
+        if (letter.isupper()):
+            text += chr(ord(letter) - shift)
+        else:
+            text += chr(ord(letter) - shift)
+
     return text
